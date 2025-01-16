cf9f642c4d502bfaa33a8fd97fe01b36aefb4de8
Parthasarathy <parthasarathy3737@gmail.com>
2025-01-16 09:36:27 +0530

    feat: add target value in op of BinarySearch

--- src/com/java/algorithms/searching/OrderAgnosticBS.java
+++ src/com/java/algorithms/searching/OrderAgnosticBS.java
@@ -6,10 +6,10 @@ public class OrderAgnosticBS {
     public static void main(String[] args) {
         int [] arr={-1,0,23,43,54,75,90};
         int target=23;
-        System.out.println("the index is "+searching(arr,target));
+        System.out.println("the "+target+" in index is "+searching(arr,target));
         int []arr1={90,80,40,20,10,0,-10};
         target=80;
-        System.out.println("the index is "+searching(arr1,target));
+        System.out.println("the "+target+" in index is "+searching(arr1,target));
     }
 
     static int searching(int []arr,int target){
