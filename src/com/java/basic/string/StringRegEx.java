982422012827a0bf02067d6c0279146efae761d9
Parthasarathy <parthasarathy3737@gmail.com>
2025-03-19 09:35:20 +0530

    chore:modifying regex

--- src/com/java/basic/string/StringRegEx.java
+++ src/com/java/basic/string/StringRegEx.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 public class StringRegEx {
     public static void main(String[] args) {
         System.out.println(Pattern.matches(".*\\bhello\\b.*","hello world"));
-        System.out.println();
+        System.out.println(Pattern.matches("a*","ab"));
+        System.out.println(Pattern.matches("a*",""));
+        System.out.println(Pattern.matches("\\d{1,2}-\\d{1,2}-\\d{4}","20-11-2004"));
     }
 }
